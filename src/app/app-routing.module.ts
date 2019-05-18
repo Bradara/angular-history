c72493acf12481c85cb798991e79867567f7e0db
Bradara <atanas.nikolov74@gmail.com>
2019-05-18 19:53:14 +0300

    11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticleDatailsComponent } from './article-datails/article-datails.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'angular-history/angular-history/article/:id', component: ArticleDatailsComponent }
+  { path: 'angular-history/article/:id', component: ArticleDatailsComponent },
+  { path: '**', redirectTo: 'angular-history/article/1', pathMatch: 'full' }
 ];
 
 @NgModule({
