898cefb5150de7a9c3fd54e302038a296678ef72
Bradara <atanas.nikolov74@gmail.com>
2019-05-19 07:38:38 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticleDatailsComponent } from './article-datails/article-datails.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'article/:id', component: ArticleDatailsComponent },
-  { path: '**', redirectTo: '/article/1', pathMatch: 'full' }
+  { path: '', redirectTo: 'article/1', pathMatch: 'full' }
 ];
 
 @NgModule({
